68740a5983f2ffdc041a3425347aa0fe1dd08b8a
gil46 <gil4690@naver.com>
2023-09-08 17:01:42 +0900

    first commit

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,7 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -15,13 +16,31 @@ public class MemberService {
 
     public Long join(Member member){
         //같은 이름이 있는 중복 회원x
-        Optional<Member> result =  memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.")
-        });
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
 
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
 
+        }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMember(){
+            return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findByID(memberId);
+    }
 }
+
+
+
